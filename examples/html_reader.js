a7768643c33e353643a0ec8945f1fd9b39460da3
Alexander Yukal <yukal@email.ua>
2022-01-19 23:06:06 +0200

    add example for HtmlReader

---
+++ examples/html_reader.js
@@ -0,0 +1,66 @@
+const HtmlReader = require('../lib/HtmlReader');
+
+const parseSpecificHtmlPage = (content) => {
+  const parser = HtmlReader.from(content);
+
+  parser
+    // Find all the tags <table>
+    .findAll('table')
+
+    // Save finded data to a dictionary {tables[]}
+    // It will save only 4th and 5th items
+    .pinItems('tables', [4, 5])
+
+    // Chose saved data from a dictionary: {tables[], , ...}
+    .fromPin('tables')
+
+    // Take first element from an array
+    .item(0)
+
+    // Find all the tags <tr> inside tables[ 0:tr[], 1:tr[] ]
+    .findAll('tr')
+
+    // Save only first 3 items
+    .pinItems('rows', [0, 1, 2])
+
+    .fromPin('tables').item(1)
+
+    // Find all images with class name "image_galary"
+    // and return only values with "src" attribute
+    .findAll('img.image_galary[src]')
+    .pin('images')
+
+    // Remove data from the dictionary
+    .unpin('tables')
+
+    // See object state in a console
+    .dump()
+
+    .fromPin('rows').item(0).findOne('h1').pin('title')
+    .fromPin('rows').item(1).findOne('#info').pin('info')
+    .fromPin('rows').item(2).findOne('#data').pin('data')
+    .unpin('rows')
+
+    // See object state in a console
+    .dump()
+
+    .fromPin('data').findOne('#contacts').pin('contacts')
+  ;
+
+  const title = parser.getInnerData('title');
+  const info = parser.getInnerData('info');
+  const contacts = parser.getInnerData('contacts');
+  const images = parser.data('images');
+
+  // Clean up
+  parser.flushResults();
+
+  return {
+    title,
+    info,
+    contacts,
+    images,
+  };
+};
+
+parseSpecificHtmlPage(htmlContent);
