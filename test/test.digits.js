89669a0396b70d563259e3e569ac85c70a24d2b7
Alexander Yukal <yukal@email.ua>
2022-01-19 20:04:20 +0200

    change space indentation

--- test/test.digits.js
+++ test/test.digits.js
@@ -15,102 +15,102 @@ const BRONTO_BYTE = Math.pow(KILO_BYTE, 9);
 const valCCARDN16 = '1234567890123456';
 const resCCARDN16 = '1234 5678 9012 3456';
 const checkDigits = [
-    ['000','6400','zeros'],
-    ['011','6501','ones'],
-    ['022','6602','deuces'],
-    ['033','6703','triplets'],
-    ['044','6804','fours'],
-    ['055','6905','fives'],
-    ['066','6a06','sixes'],
-    ['077','6b07','sevens'],
-    ['088','6c08','eights'],
-    ['099','6d09','nines'],
-    [
-        '00010203040506070809',
-        '6465666768696a6b6c6d',
-        ''
-    ]
+  ['000', '6400', 'zeros'],
+  ['011', '6501', 'ones'],
+  ['022', '6602', 'deuces'],
+  ['033', '6703', 'triplets'],
+  ['044', '6804', 'fours'],
+  ['055', '6905', 'fives'],
+  ['066', '6a06', 'sixes'],
+  ['077', '6b07', 'sevens'],
+  ['088', '6c08', 'eights'],
+  ['099', '6d09', 'nines'],
+  [
+    '00010203040506070809',
+    '6465666768696a6b6c6d',
+    ''
+  ]
 ];
 
 const checkBytesToShortSize = [
-    ['98b', 'byte', 98],
-    ['1Kb', 'kilo', KILO_BYTE],
-    ['1Mb', 'mega', MEGA_BYTE],
-    ['1Gb', 'giga', GIGA_BYTE],
-    ['1Tb', 'tera', TERA_BYTE],
-    ['1Pb', 'peta', PETA_BYTE],
-    ['1Eb', 'exa', EXA_BYTE],
-    ['1Zb', 'zetta', ZETTA_BYTE],
-    ['1Yb', 'yotta', YOTTA_BYTE],
-    ['1Bb', 'bronto', BRONTO_BYTE],
+  ['98b', 'byte', 98],
+  ['1Kb', 'kilo', KILO_BYTE],
+  ['1Mb', 'mega', MEGA_BYTE],
+  ['1Gb', 'giga', GIGA_BYTE],
+  ['1Tb', 'tera', TERA_BYTE],
+  ['1Pb', 'peta', PETA_BYTE],
+  ['1Eb', 'exa', EXA_BYTE],
+  ['1Zb', 'zetta', ZETTA_BYTE],
+  ['1Yb', 'yotta', YOTTA_BYTE],
+  ['1Bb', 'bronto', BRONTO_BYTE],
 ];
 
 const checkBytesToFixedSize = [
-    ['98.0b', 'byte', 98],
-    ['1.0Kb', 'kilo', KILO_BYTE],
-    ['1.0Mb', 'mega', MEGA_BYTE],
-    ['1.0Gb', 'giga', GIGA_BYTE],
-    ['1.0Tb', 'tera', TERA_BYTE],
-    ['1.0Pb', 'peta', PETA_BYTE],
-    ['1.0Eb', 'exa', EXA_BYTE],
-    ['1.0Zb', 'zetta', ZETTA_BYTE],
-    ['1.0Yb', 'yotta', YOTTA_BYTE],
-    ['1.0Bb', 'bronto', BRONTO_BYTE],
+  ['98.0b', 'byte', 98],
+  ['1.0Kb', 'kilo', KILO_BYTE],
+  ['1.0Mb', 'mega', MEGA_BYTE],
+  ['1.0Gb', 'giga', GIGA_BYTE],
+  ['1.0Tb', 'tera', TERA_BYTE],
+  ['1.0Pb', 'peta', PETA_BYTE],
+  ['1.0Eb', 'exa', EXA_BYTE],
+  ['1.0Zb', 'zetta', ZETTA_BYTE],
+  ['1.0Yb', 'yotta', YOTTA_BYTE],
+  ['1.0Bb', 'bronto', BRONTO_BYTE],
 ];
 
 // ..............................
 // digitsToBytes
 
 describe('digits to bytes', () => {
-    checkDigits.map(([value, expected, groupName]) => {
-        it(`"${value}" -> "${expected}" ${groupName}`, () => {
-            const data = digits.digitsToBytes(value);
-            expect(data.toString('hex')).equal(expected)
-        });
+  checkDigits.map(([value, expected, groupName]) => {
+    it(`"${value}" -> "${expected}" ${groupName}`, () => {
+      const data = digits.digitsToBytes(value);
+      expect(data.toString('hex')).equal(expected)
     });
+  });
 });
 
 // ..............................
 // bytesToDigits
 
 describe('bytes to digits', () => {
-    const checkReversed = checkDigits.map(a => [ a[1], a[0], a[2] ]);
+  const checkReversed = checkDigits.map(a => [a[1], a[0], a[2]]);
 
-    checkReversed.map(([value, expected, groupName]) => {
-        it(`"${value}" -> "${expected}" ${groupName}`, () => {
-            expect(digits.bytesToDigits(Buffer.from(value, 'hex'))).equal(expected);
-        });
+  checkReversed.map(([value, expected, groupName]) => {
+    it(`"${value}" -> "${expected}" ${groupName}`, () => {
+      expect(digits.bytesToDigits(Buffer.from(value, 'hex'))).equal(expected);
     });
+  });
 });
 
 // ..............................
 // bytesToShortSize
 
 describe('bytes to short size', () => {
-    checkBytesToShortSize.map(([expected, name, value]) => {
-        it(`1 ${name} = "${expected}"`, () => {
-            expect(digits.bytesToShortSize(value)).equal(expected);
-        });
+  checkBytesToShortSize.map(([expected, name, value]) => {
+    it(`1 ${name} = "${expected}"`, () => {
+      expect(digits.bytesToShortSize(value)).equal(expected);
     });
+  });
 });
 
 // ..............................
 // bytesToFixedSize
 
 describe('bytes to fixed size', () => {
-    checkBytesToFixedSize.map(([expected, name, value]) => {
-        it(`1 ${name} = "${expected}"`, () => {
-            expect(digits.bytesToFixedSize(value)).equal(expected);
-        });
+  checkBytesToFixedSize.map(([expected, name, value]) => {
+    it(`1 ${name} = "${expected}"`, () => {
+      expect(digits.bytesToFixedSize(value)).equal(expected);
     });
+  });
 });
 
 // ..............................
 // getCreditCardMaskN16
 
 describe('credit card mask N16', () => {
-    it(`"${valCCARDN16}" -> "${resCCARDN16}"`, () => {
-        const data = digits.getCreditCardMaskN16(valCCARDN16);
-        expect(data).equal(resCCARDN16);
-    });
+  it(`"${valCCARDN16}" -> "${resCCARDN16}"`, () => {
+    const data = digits.getCreditCardMaskN16(valCCARDN16);
+    expect(data).equal(resCCARDN16);
+  });
 });
--- test/test.xml.js
+++ test/test.xml.js
@@ -4,165 +4,165 @@ const XML = require('../lib/xml');
 const checkData = {};
 
 before(async () => {
-    const Fs = require('fs');
-    const Path = require('path');
+  const Fs = require('fs');
+  const Path = require('path');
 
-    const PATH_DATA = Path.join(process.cwd(), './test/data');
-    const xmlFilePath = Path.join(PATH_DATA, '1.xml');
+  const PATH_DATA = Path.join(process.cwd(), './test/data');
+  const xmlFilePath = Path.join(PATH_DATA, '1.xml');
 
-    checkData.xmlContent = await Fs.promises.readFile(xmlFilePath, 'utf8');
+  checkData.xmlContent = await Fs.promises.readFile(xmlFilePath, 'utf8');
 });
 
 // ..............................
 // check xml
 
 describe('xml parsing', () => {
-    let xml1;
+  let xml1;
 
-    it('parsed XML should be an object', () => {
-        xml1 = XML.parse(checkData.xmlContent);
-        expect(xml1).an('object').not.empty;
+  it('parsed XML should be an object', () => {
+    xml1 = XML.parse(checkData.xmlContent);
+    expect(xml1).an('object').not.empty;
+  });
+
+  describe('general', () => {
+    it('root element', () => {
+      expect(xml1).property('data');
     });
 
-    describe('general', () => {
-        it('root element', () => {
-            expect(xml1).property('data');
-        });
-
-        it('cleaned up after parsing', () => {
-            expect(xml1.data).not.to.have.property(XML._POCKET_NAME);
-        });
-
-        it('header with attributes', () => {
-            expect(xml1).property('@version', '1.0');
-            expect(xml1).property('@encoding', 'UTF-8');
-        });
+    it('cleaned up after parsing', () => {
+      expect(xml1.data).not.to.have.property(XML._POCKET_NAME);
     });
 
-    // ..............
-    // Empties
+    it('header with attributes', () => {
+      expect(xml1).property('@version', '1.0');
+      expect(xml1).property('@encoding', 'UTF-8');
+    });
+  });
 
-    describe('empties', () => {
-        it('nullable data', () => {
-            expect(xml1.data).property('nullable', null);
-        });
+  // ..............
+  // Empties
 
-        it('empty object (single tag)', () => {
-            expect(xml1.data).property('empty_object_single').eql({});
-        });
-
-        it('empty object (paired tag)', () => {
-            expect(xml1.data).property('empty_object_paired', null);
-        });
-
-        it('array with empty items', () => {
-            expect(xml1.data).property('empty_items_array').eql([null, null]);
-        });
-
-        it('array with empty objects', () => {
-            expect(xml1.data).property('empty_objects_array').eql([{}, {}]);
-        });
+  describe('empties', () => {
+    it('nullable data', () => {
+      expect(xml1.data).property('nullable', null);
     });
 
-    // ..............
-    // Arrays
-
-    describe('arrays', () => {
-        it('array with filled items', () => {
-            expect(xml1.data).property('array').eql([
-                "item1",
-                "item2",
-                "item3",
-            ]);
-        });
-
-        it('array with filled objects', () => {
-            expect(xml1.data).property('array_of_objects').eql([
-                {
-                    "attr1": "val",
-                    "attr2": "val"
-                },
-                {
-                    "attr1": "val",
-                    "attr2": "val"
-                },
-                {
-                    "attr1": "val",
-                    "attr2": "val"
-                }
-            ]);
-        });
+    it('empty object (single tag)', () => {
+      expect(xml1.data).property('empty_object_single').eql({});
     });
 
-    // ..............
-    // Objects
-
-    describe('objects', () => {
-        it('object with filled attributes', () => {
-            expect(xml1.data).property('object').eql({
-                "name": "AGENT_JUPITER",
-                "qmgr": "QM_JUPITER"
-            });
-        });
-
-        it('object with complex attributes', () => {
-            expect(xml1.data).property('object_complex').eql({
-                "array": [
-                    {
-                        "name": "item1"
-                    },
-                    {
-                        "@name": "item2",
-                        "data": "item2"
-                    },
-                    "item3",
-                    "item4"
-                ],
-                "some": "some",
-                "data": {
-                    "some": "data"
-                }
-            });
-        });
-
-        it('object with text and array', () => {
-            expect(xml1.data).property('object_with_text1').eql({
-                "array": [
-                    "item1",
-                    "item2",
-                    "item3"
-                ],
-                "_innerText": "text <array>item1</array> text <array>item2</array> text <array>item3</array> text",
-                "_outerText": "<object_with_text1>\n        text <array>item1</array> text <array>item2</array> text <array>item3</array> text\n    </object_with_text1>"
-            });
-        });
-
-        it('object with text and attributes', () => {
-            expect(xml1.data).property('object_with_text2').eql({
-                "item1": "item1",
-                "item2": "item2",
-                "item3": "item3",
-                "_innerText": "text <item1>item1</item1> text <item2>item2</item2> text <item3>item3</item3> text",
-                "_outerText": "<object_with_text2>\n        text <item1>item1</item1> text <item2>item2</item2> text <item3>item3</item3> text\n    </object_with_text2>"
-            });
-        });
+    it('empty object (paired tag)', () => {
+      expect(xml1.data).property('empty_object_paired', null);
     });
 
-    // ..............
-    // Texts
-
-    describe('texts', () => {
-        it('text data', () => {
-            expect(xml1.data).property('text', 'text data');
-        });
-
-        it('text with attributes', () => {
-            expect(xml1.data).property('text_with_attributes').eql({
-                "@id": "baf9df73",
-                "@type": "txt",
-                "data": "text data"
-            });
-        });
+    it('array with empty items', () => {
+      expect(xml1.data).property('empty_items_array').eql([null, null]);
     });
+
+    it('array with empty objects', () => {
+      expect(xml1.data).property('empty_objects_array').eql([{}, {}]);
+    });
+  });
+
+  // ..............
+  // Arrays
+
+  describe('arrays', () => {
+    it('array with filled items', () => {
+      expect(xml1.data).property('array').eql([
+        "item1",
+        "item2",
+        "item3",
+      ]);
+    });
+
+    it('array with filled objects', () => {
+      expect(xml1.data).property('array_of_objects').eql([
+        {
+          "attr1": "val",
+          "attr2": "val"
+        },
+        {
+          "attr1": "val",
+          "attr2": "val"
+        },
+        {
+          "attr1": "val",
+          "attr2": "val"
+        }
+      ]);
+    });
+  });
+
+  // ..............
+  // Objects
+
+  describe('objects', () => {
+    it('object with filled attributes', () => {
+      expect(xml1.data).property('object').eql({
+        "name": "AGENT_JUPITER",
+        "qmgr": "QM_JUPITER"
+      });
+    });
+
+    it('object with complex attributes', () => {
+      expect(xml1.data).property('object_complex').eql({
+        "array": [
+          {
+            "name": "item1"
+          },
+          {
+            "@name": "item2",
+            "data": "item2"
+          },
+          "item3",
+          "item4"
+        ],
+        "some": "some",
+        "data": {
+          "some": "data"
+        }
+      });
+    });
+
+    it('object with text and array', () => {
+      expect(xml1.data).property('object_with_text1').eql({
+        "array": [
+          "item1",
+          "item2",
+          "item3"
+        ],
+        "_innerText": "text <array>item1</array> text <array>item2</array> text <array>item3</array> text",
+        "_outerText": "<object_with_text1>\n        text <array>item1</array> text <array>item2</array> text <array>item3</array> text\n    </object_with_text1>"
+      });
+    });
+
+    it('object with text and attributes', () => {
+      expect(xml1.data).property('object_with_text2').eql({
+        "item1": "item1",
+        "item2": "item2",
+        "item3": "item3",
+        "_innerText": "text <item1>item1</item1> text <item2>item2</item2> text <item3>item3</item3> text",
+        "_outerText": "<object_with_text2>\n        text <item1>item1</item1> text <item2>item2</item2> text <item3>item3</item3> text\n    </object_with_text2>"
+      });
+    });
+  });
+
+  // ..............
+  // Texts
+
+  describe('texts', () => {
+    it('text data', () => {
+      expect(xml1.data).property('text', 'text data');
+    });
+
+    it('text with attributes', () => {
+      expect(xml1.data).property('text_with_attributes').eql({
+        "@id": "baf9df73",
+        "@type": "txt",
+        "data": "text data"
+      });
+    });
+  });
 
 });
