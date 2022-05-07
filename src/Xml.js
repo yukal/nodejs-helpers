113d2659e6401bdbe7221418d0f04a2bbd6876dd
Alexander Yukal <yukal@email.ua>
2022-05-07 13:15:25 +0300

    implementation of stringify

--- src/Xml.js
+++ src/Xml.js
@@ -43,6 +43,14 @@
 const XML_ATTR_AT = '@';
 const XML_ATTR_TEXT = '&text';
 
+const XML_DEFAULT_VERSION = '1.0';
+const XML_DEFAULT_ENCODING = 'UTF-8';
+const XML_DEFAULT_BR = '\n';
+const XML_DEFAULT_INDENT = '  ';
+
+const T_OBJECT = 1;
+const T_ARRAY = 2;
+
 /**
  * XML
  *
@@ -61,7 +69,7 @@ const XML = (textOrObject) => {
 
 /**
  * XML.parse
- * Parse the XML text and return the result as a data object.
+ * Parse an XML text and return the result as a data object.
  *
  * @param {string} xmlText 
  * @returns object
@@ -122,8 +130,163 @@ XML.parse = (xmlText) => {
   return tree;
 };
 
-XML.stringify = (xml) => {
-  throw new Error('xml.stringify() not implemented yet');
+XML.stringify = (object, options) => {
+  if (Type(object) !== 'object') {
+    return '';
+  }
+
+  const clone = { ...object };
+  const initiatedOptions = stringifyInitOptions(options);
+
+  const params = stringifyParams(clone, initiatedOptions);
+  const body = XML.stringifyBody(clone, initiatedOptions);
+
+  return `<?xml ${params}?>${body}`;
+};
+
+XML.stringifyBody = (object, options) => {
+  if (Type(object) !== 'object') {
+    return '';
+  }
+
+  const initiatedOptions = stringifyInitOptions(options);
+
+  const pocket = {};
+  let entries = Object.keys(object);
+  let hasEntries = entries.length > 0;
+  let indentDepth = 0;
+  let depth = 0;
+  let body = '';
+
+  pocket[depth] = {
+    key: 'root',
+    val: object,
+    entries,
+    attribs: '',
+    body: '',
+    hasChilds: false,
+    type: T_OBJECT,
+  };
+
+  while (hasEntries) {
+    const pocketItem = pocket[depth];
+    const indent = calcIndentation(indentDepth, initiatedOptions);
+
+    if (entries.length > 0) {
+      const key = entries[0];
+      const val = pocketItem.val[key];
+
+      if (key.startsWith(XML_ATTR_AT)) {
+        pocketItem.attribs += ` ${key.slice(1)}="${val}"`;
+
+        delete pocketItem.val[key];
+        entries.shift();
+
+        continue;
+      }
+
+      if (key.startsWith(XML_ATTR_TEXT)) {
+        const indent = calcIndentation(indentDepth, initiatedOptions);
+        const body = Array.isArray(val)
+          ? val.join(indent) : val;
+
+        pocketItem.body += `${indent}${body}`;
+
+        delete pocketItem.val[key];
+        entries.shift();
+
+        continue;
+      }
+
+      switch (Type(val)) {
+        case 'object':
+          pocketItem.hasChilds = true;
+          entries = Object.keys(val);
+
+          depth++;
+          indentDepth++;
+
+          pocket[depth] = {
+            key,
+            val,
+            entries,
+            attribs: '',
+            body: '',
+            hasChilds: false,
+            type: T_OBJECT,
+          };
+          break;
+
+        case 'array':
+          pocketItem.hasChilds = true;
+          entries = Object.keys(val);
+
+          depth++;
+
+          pocket[depth] = {
+            key,
+            val,
+            entries,
+            attribs: '',
+            body: '',
+            hasChilds: false,
+            type: T_ARRAY,
+          };
+          break;
+
+        case 'boolean':
+        case 'number':
+        case 'bigint':
+        case 'string':
+          const tagName = pocketItem.type === T_ARRAY
+            ? pocketItem.key : key;
+
+          pocketItem.body += `${indent}<${tagName}>${val}</${tagName}>`;
+          entries.shift();
+          break;
+
+        default:
+          entries.shift();
+          break;
+      }
+    } else {
+      if (depth > 0) {
+        const { key, type, body, attribs, hasChilds } = pocket[depth];
+
+        delete pocket[depth];
+        depth--;
+
+        const pocketItem = pocket[depth];
+        entries = pocketItem.entries;
+        entries.shift();
+
+        if (type === T_ARRAY) {
+          pocketItem.body += body;
+        }
+
+        if (type === T_OBJECT) {
+          indentDepth--;
+
+          const indent = calcIndentation(indentDepth, initiatedOptions);
+          const tagName = pocketItem.type === T_ARRAY
+            ? pocketItem.key : key;
+
+          pocketItem.body += body.length || hasChilds
+            ? `${indent}<${tagName}${attribs}>${body}${indent}</${tagName}>`
+            : `${indent}<${tagName}${attribs} />`;
+        }
+
+        continue;
+      }
+
+      body = pocket[depth].body;
+      delete pocket[depth];
+
+      hasEntries = false;
+    }
+  }
+
+  return body;
 };
 
 XML.parseAttributes = (params, prefix = '') => {
@@ -321,15 +484,66 @@ const xmlParseOpenedPairedTag = (options) => {
   // }
 };
 
-const getIndent = (cnt) => cnt > 0 ? ' '.repeat(cnt * 2) : '';
-const logTree = (depth, tagName, params) => {
-  const indent = getIndent(depth);
+const stringifyParams = (clone, options) => {
+  let xmlParams = '';
 
-  const args = depth > 0
-    ? [indent, depth, tagName, params.trim()]
-    : [depth, tagName, params.trim()];
+  if (!clone.hasOwnProperty('@version')) {
+    clone['@version'] = options.version != undefined
+      ? options.version
+      : XML_DEFAULT_VERSION;
+  }
 
-  console.log(...args);
+  if (!clone.hasOwnProperty('@encoding')) {
+    clone['@encoding'] = options.encoding != undefined
+      ? options.encoding
+      : XML_DEFAULT_ENCODING;
+  }
+
+  for (const key in clone) {
+    if (key.startsWith(XML_ATTR_AT)) {
+      const lowerKey = key.slice(1).toLowerCase();
+      xmlParams += ` ${lowerKey}="${clone[key]}"`;
+
+      delete clone[key];
+    }
+  }
+
+  return xmlParams.trim();
 };
 
-module.exports = XML;
+const stringifyInitOptions = (options) => {
+  const defaultOptions = {
+    version: XML_DEFAULT_VERSION,
+    encoding: XML_DEFAULT_ENCODING,
+    breakLine: XML_DEFAULT_BR,
+    indentation: XML_DEFAULT_INDENT,
+  };
+
+  const initOptions = Type(options) === 'object'
+    ? { ...options } : {};
+
+  for (const key in defaultOptions) {
+    if (!initOptions.hasOwnProperty(key)) {
+      initOptions[key] = defaultOptions[key];
+    }
+  }
+
+  return initOptions;
+};
+
+const calcIndentation = (depth, options) => {
+  const { breakLine: br } = options;
+
+  const space = depth > 0
+    ? ' '.repeat(depth * 2)
+    : '';
+
+  return `${br}${space}`;
+};
+
+const Type = (obj) => {
+  const signature = Object.prototype.toString.call(obj);
+  return signature.slice(8, -1).toLowerCase();
+}
+
+module.exports = Object.freeze(XML);
